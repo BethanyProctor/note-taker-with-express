209cc8cfd9380e0616609e2fd1baa469a51cc5d9
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-06-03 15:43:21 -0400

    test

---
+++ routes/api-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+router.get('/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+router.post('/notes', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(api_routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use("/api", api_routes);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+  );
+
+  app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  );
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
